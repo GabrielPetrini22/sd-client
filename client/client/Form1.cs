a00bb23511b7d15b40158d7b000ce8815ed05471
GabrielPetrini22 <gabrielpetrini22@gmail.com>
2016-06-15 17:39:41 -0300

    backup
    backup

--- client/client/Form1.cs
+++ client/client/Form1.cs
@@ -32,6 +32,49 @@ namespace client
         //StreamReader sr;
         //Thread th;
         string ip;
+        private const int TAMANHO_BUFFER = 10000;
+        private int requisicoes;
+        //mensagem que o cliente manda para o servidor
+        private string mensagemCliente;
+        //mensagem que o servidor manda ao cliente
+        private string respostaServidor;
+        //Socket do servidor
+        private TcpListener servidor;
+        //Socket do cliente
+        private TcpClient cliente;
+        void Servidor()
+        {
+            this.servidor = new TcpListener(IPAddress.Any, 4243);
+            this.cliente = default(TcpClient);
+            this.servidor.Start();
+            this.cliente = servidor.AcceptTcpClient();
+            this.requisicoes = 0;
+            this.respostaServidor = "";
+        }
+        public void Run()
+        {
+            this.requisicoes++;
+            NetworkStream netStream = cliente.GetStream();
+            byte[] recebido = new byte[TAMANHO_BUFFER];
+            //recebe a mensagem do cliente
+            netStream.Read(recebido, 0, (int)cliente.ReceiveBufferSize);
+            //converte bytes em string
+            this.mensagemCliente = Encoding.ASCII.GetString(recebido);
+            /* reduz a string deixando de fora os caracteres
+             * adicionados durante o processo de conversão bytes->string */
+            this.mensagemCliente = this.mensagemCliente.Substring(0, this.mensagemCliente.IndexOf("$"));
+
+            ///* define a resposta do servidor
+            // * manda para o cliente a mensagem recebida
+            // * convertida em letras maiusculas */
+            //this.respostaServidor = "Resposta do Servidor " + Convert.ToString(requisicoes) + ": " +
+            //+this.mensagemCliente.ToUpperInvariant();
+
+            //Byte[] enviado = Encoding.ASCII.GetBytes(this.RespostaServidor);
+            ////envia a resposta em bytes ao cliente
+            //netStream.Write(enviado, 0, enviado.Length);
+            //netStream.Flush();
+        }
 
         //void ReceivedText()
         //{
