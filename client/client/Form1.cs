546b5665cafb785100aa5b093334e865dba4dad5
GabrielPetrini22 <gabrielpetrini22@gmail.com>
2016-06-16 17:25:42 -0300

    backup
    backup

--- client/client/Form1.cs
+++ client/client/Form1.cs
@@ -12,6 +12,7 @@ using System.Net.Sockets;
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Threading;
+using client.Classes;
 
 namespace client
 {
@@ -21,84 +22,47 @@ namespace client
         {
             InitializeComponent();
         }
+
+        //cria o servidor
+
         MemoryStream ms;
         TcpClient client;
         NetworkStream ns;
         BinaryWriter br;
-        ////server
-        //Socket skt;
-        //TcpListener t1;
-        //NetworkStream ns2;
-        //StreamReader sr;
-        //Thread th;
+        //server
+        Socket skt;
+        TcpListener t1;
+        NetworkStream ns2;
+        StreamReader sr;
+        Thread th;
+        TcpClient cliente;
+
         string ip;
-        private const int TAMANHO_BUFFER = 10000;
-        private int requisicoes;
-        //mensagem que o cliente manda para o servidor
-        private string mensagemCliente;
-        //mensagem que o servidor manda ao cliente
-        private string respostaServidor;
-        //Socket do servidor
-        private TcpListener servidor;
-        //Socket do cliente
-        private TcpClient cliente;
-        void Servidor()
+        void ReceivedText()
         {
-            this.servidor = new TcpListener(IPAddress.Any, 4243);
-            this.cliente = default(TcpClient);
-            this.servidor.Start();
-            this.cliente = servidor.AcceptTcpClient();
-            this.requisicoes = 0;
-            this.respostaServidor = "";
+            try
+            {
+                t1 = new TcpListener(4243);
+                t1.Start();
+                skt = t1.AcceptSocket();
+                ns2 = new NetworkStream(skt);
+                byte[] recebido = new byte[10000];
+                ns2.Read(recebido, 0, (int)cliente.ReceiveBufferSize);
+                string line;
+                line = Encoding.ASCII.GetString(recebido);
+                line = line.Substring(0, line.IndexOf("$"));
+                textBox3.Text = line;
+                t1.Stop();
+                if (skt.Connected == true)
+                {
+                    while (true)
+                    {
+                        ReceivedText();
+                    }
+                }
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
         }
-        public void Run()
-        {
-            this.requisicoes++;
-            NetworkStream netStream = cliente.GetStream();
-            byte[] recebido = new byte[TAMANHO_BUFFER];
-            //recebe a mensagem do cliente
-            netStream.Read(recebido, 0, (int)cliente.ReceiveBufferSize);
-            //converte bytes em string
-            this.mensagemCliente = Encoding.ASCII.GetString(recebido);
-            /* reduz a string deixando de fora os caracteres
-             * adicionados durante o processo de conversão bytes->string */
-            this.mensagemCliente = this.mensagemCliente.Substring(0, this.mensagemCliente.IndexOf("$"));
-
-            ///* define a resposta do servidor
-            // * manda para o cliente a mensagem recebida
-            // * convertida em letras maiusculas */
-            //this.respostaServidor = "Resposta do Servidor " + Convert.ToString(requisicoes) + ": " +
-            //+this.mensagemCliente.ToUpperInvariant();
-
-            //Byte[] enviado = Encoding.ASCII.GetBytes(this.RespostaServidor);
-            ////envia a resposta em bytes ao cliente
-            //netStream.Write(enviado, 0, enviado.Length);
-            //netStream.Flush();
-        }
-
-        //void ReceivedText()
-        //{
-        //    try
-        //    {
-        //        t1 = new TcpListener(4243);
-        //        t1.Start();
-        //        skt = t1.AcceptSocket();
-        //        ns2 = new NetworkStream(skt);
-        //        byte[] buffer = new byte[10];
-        //        ns.Read(buffer, 0, 1);
-        //        string line = Encoding.UTF8.GetString(buffer);
-        //        textBox3.Text = line;
-        //        t1.Stop();
-        //        if (skt.Connected == true)
-        //        {
-        //            while (true)
-        //            {
-        //                ReceivedText();
-        //            }
-        //        }
-        //    }
-        //    catch (Exception ex) { MessageBox.Show(ex.Message); }
-        //}
         //obtem endereço local
         //string GetIpAdress()
         //{
@@ -139,14 +103,15 @@ namespace client
                 client.Close();
 
             }
-            catch(Exception ex) { MessageBox.Show(ex.Message); }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            //th = new Thread(new ThreadStart(ReceivedText));
-            //th.Start();
+            th = new Thread(new ThreadStart(ReceivedText));
+            th.Start();
             //textBox1.Text = GetIpAdress();
+
         }
     }
 }
