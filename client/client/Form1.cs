ca7d66b4733b71380fa662982567681dd4a2cf20
GabrielPetrini22 <gabrielpetrini22@gmail.com>
2016-06-17 15:44:19 -0300

    back
    kcab

--- client/client/Form1.cs
+++ client/client/Form1.cs
@@ -12,7 +12,6 @@ using System.Net.Sockets;
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Threading;
-using client.Classes;
 
 namespace client
 {
@@ -23,35 +22,49 @@ namespace client
             InitializeComponent();
         }
 
-        //cria o servidor
-
+        //Cliente, enviar o arquivo
         MemoryStream ms;
         TcpClient client;
+        TcpClient client2 = new TcpClient();
         NetworkStream ns;
         BinaryWriter br;
-        //server
+        //receber mensagem
         Socket skt;
         TcpListener t1;
         NetworkStream ns2;
-        StreamReader sr;
         Thread th;
-        TcpClient cliente;
-
-        string ip;
-        void ReceivedText()
+        string cabecalho;
+        void ReceivedText()//recebe a String da placa ou não reconhecida
         {
             try
             {
-                t1 = new TcpListener(4243);
+                t1 = new TcpListener(4250);
                 t1.Start();
                 skt = t1.AcceptSocket();
                 ns2 = new NetworkStream(skt);
-                byte[] recebido = new byte[10000];
-                ns2.Read(recebido, 0, (int)cliente.ReceiveBufferSize);
-                string line;
-                line = Encoding.ASCII.GetString(recebido);
-                line = line.Substring(0, line.IndexOf("$"));
-                textBox3.Text = line;
+                byte[] buffer = new byte[10000];
+                ns2.Read(buffer, 0, 1000);
+                //Recebe o cabeçalho
+                cabecalho = Encoding.UTF8.GetString(buffer);
+                cabecalho = cabecalho.Substring(0, cabecalho.IndexOf("$"));
+                //Identifica a ação e verifica o que fazer
+                if (cabecalho.Substring(0, 1).Equals("4"))
+                {
+                    MessageBox.Show("Placa não reconhecida", "Erro", MessageBoxButtons.OK);
+                }
+                else if (cabecalho.Substring(0, 1).Equals("3"))
+                {
+                    if (MessageBox.Show("A placa reconhecida é: " + cabecalho.Substring(1, 8), "Confirmação", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                    {
+                        cabecalho = "21";
+                    }
+                    else
+                    {
+                        cabecalho = "20";
+                    }
+                    //retorna o cabeçalho ao servidor
+
+                }
                 t1.Stop();
                 if (skt.Connected == true)
                 {
@@ -101,17 +114,15 @@ namespace client
                 br.Close();
                 ns.Close();
                 client.Close();
-
             }
-            catch (Exception ex) { MessageBox.Show(ex.Message); }
+            catch(Exception ex) { MessageBox.Show(ex.Message); }
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+           
             th = new Thread(new ThreadStart(ReceivedText));
             th.Start();
-            //textBox1.Text = GetIpAdress();
-
         }
     }
 }
